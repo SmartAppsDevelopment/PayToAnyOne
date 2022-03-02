b39979857f5cd58009d5ed2d9225ba047f45548a
SmartAppsDevelopment <umeroffice786@gmail.com>
2022-03-02 16:42:31 +0500

    First 3 Screen Of task completed with account selection expanded view

---
+++ app/src/main/java/com/example/templatesampleapp/ui/frag_empty/EmptyFragment.kt
@@ -0,0 +1,26 @@
+package com.example.templatesampleapp.ui.frag_empty
+
+import androidx.lifecycle.*
+import com.example.templatesampleapp.R
+import com.example.templatesampleapp.base.BaseFragment
+import com.example.templatesampleapp.base.BaseViewModel
+import com.example.templatesampleapp.databinding.FragmentMyPayeesBinding
+import com.example.templatesampleapp.helper.*
+import com.example.templatesampleapp.model.uimodel.ToolBarRef
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class EmptyFragment() :
+    BaseFragment<FragmentMyPayeesBinding>(R.layout.fragment_empty) {
+    override fun getToolbar() = ToolBarRef("Sample Frag", searchClick = {
+        showLog("1 Search Click")
+    }, userImgClick = {
+        showLog("1 Img CLick")
+    })
+
+    override val viewModel: BaseViewModel
+        get() = TODO("Not yet implemented")
+
+
+}
